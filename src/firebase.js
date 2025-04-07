83216156fd32320f81c473a310c3ad5396d4a6b6
discordthemes <sampelter183@gmail.com>
2025-04-07 14:14:38 +0100

    Create firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,28 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// ** REPLACE THIS WITH YOUR ACTUAL CONFIG OBJECT FROM FIREBASE **
+const firebaseConfig = {
+  apiKey: "your-api-key",
+  authDomain: "your-project-id.firebaseapp.com",
+  projectId: "your-project-id",
+  storageBucket: "your-project-id.appspot.com",
+  messagingSenderId: "your-messaging-sender-id",
+  appId: "your-app-id",
+  measurementId: "your-measurement-id"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Firebase Authentication and Firestore and get references to the services
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+// Export the initialized services
+export { auth, db }; 
