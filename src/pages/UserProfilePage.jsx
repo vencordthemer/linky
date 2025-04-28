816d755bae29c575c612d0cccfeb8860f17296b0
vencordthemer <sampelter183@gmail.com>
2025-04-28 17:56:41 +0100

    Added Header

--- src/pages/UserProfilePage.jsx
+++ src/pages/UserProfilePage.jsx
@@ -1,3 +1,15 @@
+/*!
+* Linky - A Linktree Clone Built with Vite and Firebase
+* Copyright (c) 2025 Vencordthemer and contributors
+*
+* This project is open-source and licensed under the MIT License.
+* You can find the source code at: https://github.com/vencordthemer/linky
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* MIT License for more details.
+*/
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 // Import Firestore functions
@@ -114,7 +126,7 @@ const UserProfilePage = () => {
                         >
                            {link.title}
                         </a>
-                        // TODO: Replace above with a proper LinkCard component
+
                     ))
                 ) : (
                     <p className="text-light">This user hasn't added any links yet.</p>
