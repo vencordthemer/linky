c90d5528b2e897ec98ae0f94882d165452733a22
vencordthemer <sampelter183@gmail.com>
2025-04-28 17:57:45 +0100

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,15 @@
-// Import the functions you need from the SDKs you need
+/*!
+* Linky - A Linktree Clone Built with Vite and Firebase
+* Copyright (c) 2025 Vencordthemer and contributors
+*
+* This project is open-source and licensed under the MIT License.
+* You can find the source code at: https://github.com/vencordthemer/linky
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* MIT License for more details.
+*/
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
