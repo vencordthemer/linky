c5b996c094489e1df8be2ce2a267a2ff1f58d176
vencordthemer <sampelter183@gmail.com>
2025-04-28 17:45:20 +0100

    Added Header

--- src/components/LinkCreateForm.jsx
+++ src/components/LinkCreateForm.jsx
@@ -1,3 +1,15 @@
+/*!
+* Linky - A Linktree Clone Built with Vite and Firebase
+* Copyright (c) 2025 Vencordthemer and contributors
+*
+* This project is open-source and licensed under the MIT License.
+* You can find the source code at: https://github.com/vencordthemer/linky
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* MIT License for more details.
+*/
 import React, { useState } from 'react';
 import { collection, addDoc, serverTimestamp } from "firebase/firestore";
 import { db } from '../firebase';
